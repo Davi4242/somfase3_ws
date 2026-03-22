02f32ab3419ae412bf5d0ea0d9d4d7074a1e2d03
Davi4242 <82727673+Davi4242@users.noreply.github.com>
2026-03-22 12:16:21 -0300

    config pro pc do davi

--- src/teste_som/teste_som/somfase3.py
+++ src/teste_som/teste_som/somfase3.py
@@ -16,8 +16,8 @@ class SoundNode(Node):
         self.get_logger().info("Subscriber de som inicializado. Aguardando mensagens...")
 
         # Caminho dos sons na Jetson
-        self.path_valor_acima = "/home/somfase3_ws/src/fase3_pkg/fase3_pkg/som_dentro_do_padrao.mp3"
-        self.path_valor_abaixo = "/home/somfase3_ws/src/fase3_pkg/fase3_pkg/som_fora_do_padrao.mp3"
+        self.path_valor_acima = "/home/ivadessal/somfase3_ws/src/teste_som/teste_som/som_dentro_do_padrao.mp3"
+        self.path_valor_abaixo = "/home/ivadessal/somfase3_ws/src/teste_som/teste_som/som_fora_do_padrao.mp3"
         self.current_process = None
     
     def listener_callback(self, msg):
